3dd7b593f00a0110983548bc680c7ab0a933cd19
Colton <wyoming454@gmail.com>
2023-01-17 21:09:53 -0700

    Update steamcmdinstall.sh

--- steamcmdinstall.sh
+++ steamcmdinstall.sh
@@ -4,6 +4,7 @@ sudo useradd -m steam
 sudo passwd steam 
 echo "steam  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/steam
 su steam
+cd ~
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 mv steamcmd.sh steamcmd
 mv steamcmd /home/steam/steamcmd
