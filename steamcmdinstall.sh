fa7d20ceb876649bc46fed9596bfd779666c9200
Colton <wyoming454@gmail.com>
2023-01-17 21:07:29 -0700

    Update steamcmdinstall.sh

--- steamcmdinstall.sh
+++ steamcmdinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install glibc.i686 libstdc++.i686 curl screen
 sudo useradd -m steam
-sudo useradd -m steam
+sudo passwd steam 
 echo "steam  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/steam
 su steam
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
